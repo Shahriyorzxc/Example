8b1f41a2707b399c8226c3731b2ef80784ddf960
Shahriyor <zxcnanevermore@gmail.com>
2024-05-03 18:26:53 +0500

    done Example

---
+++ example.py
@@ -0,0 +1,42 @@
+# def num_array(lst):
+#     ong = [0]
+#     for x in range(1):
+#         khg = lst[x]
+#     return 9.4, 4.2
+#
+#
+#
+#
+# print(num_array(['9.4', '4.2']))
+# print(num_array(['8.2', '9.0']))
+# print(num_array(['5.4', '1.5']))
+
+
+# # 2
+# def rotateByOne(number):
+#     number[0], number[4] = number[4], number[0]
+#     return number
+#
+#
+# print(rotateByOne([1, 2, 3, 4, 5]))
+# print(rotateByOne([6, 5, 8, 9, 7]))
+# print(rotateByOne([20, 15, 26, 8, 4]))
+#
+
+# 3
+# def numbers(my_list):
+#     result = []
+#     for k in my_list:
+#         for x in k:
+#             if x == k[0]:
+#                 result.append(x)
+#
+#     return sum(result)
+#
+#
+#
+# print(numbers([
+#     [1, 2, 3, 4, 5],
+#     [5, 6, 7, 8, 9],
+#     [20, 21, 34, 56, 100]
+# ]))
